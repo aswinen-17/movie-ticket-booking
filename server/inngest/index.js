3ae132dd798cb5e98f7ff9895fb9161670e401b0
aswinen-17 <enaswin17@gmail.com>
2025-11-04 02:54:41 +0530

    Updated code

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -96,12 +96,11 @@ const sendBookingConfirmationEmail = inngest.createFunction(
       subject:`Payment Confirmation : "${booking.show.movie.title}" Booked!`,
       body: `<div style ="font-family: Arial, sans-serif; line-height: 1.5;">
         <h2>Hi ${booking.user.name}, </h2>
-        <p>Your Booking for <strong style="color: #F84565;">"$
-        {booking.show.movie.title}"</strong> is confirmed.</p>
+        <p>Your Booking for <strong style="color: #F84565;">"${booking.show.movie.title}"</strong> is confirmed.</p>
         <p>
           <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', {timeZone:
             'Asia/Kolkata'})}<br/>
-            <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocalTimeString('en-US', {timeZone:
+            <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', {timeZone:
             'Asia/Kolkata'})}
             </p>
             <p>Enjoy the show! </p>
